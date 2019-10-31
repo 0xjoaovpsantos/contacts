ebf6142b713d4804a13be9b46e83d45e232b011d
Joao Victor Pereira Santos <joaovictorsantoscpv@gmail.com>
2019-10-31 13:19:04 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:app/ui/home_page.dart';
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false
+  ));
+}
+
